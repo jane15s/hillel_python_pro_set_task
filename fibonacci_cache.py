d6a93566c32e097d35910eca5947c7922576cb4e
jane <sysoievayevheniia@gmail.com>
2025-09-25 17:19:29 +0300

    edited fibonacci seq task hw12

--- fibonacci_cache.py
+++ fibonacci_cache.py
@@ -5,32 +5,31 @@ r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
 def fib_seq(stop):
     fib_list =[]
+    cached_count = 0
+    added_to_cache = 0
+
     for n in range(stop):
         cached = r.get(n)
         if cached:
             fib_list.append(int(cached))
-            if n % 1000 == 0:
-                print(f"F({n}) - from cache")
+            cached_count += 1
         else:
             next_num = n if n < 2 else fib_list[-1] + fib_list[-2]
             fib_list.append(next_num)
             r.set(n, next_num)
-            if n % 1000 == 0:
-                print(f"F({n}) - added to cache")
+            added_to_cache += 1
+
+    print(f"Total numbers requested: {stop}")
+    print(f"Taken from cache: {cached_count}")
+    print(f"Calculated for sequence: {added_to_cache}")
+
     return fib_list
 
 
 my_seq1 = fib_seq(25)
-print(my_seq1)
-
 my_seq2 = fib_seq(50)
-print(my_seq2)
-
 my_seq3 = fib_seq(100)
-print(my_seq3)
-
-my_seq4 = fib_seq(1000)
-print(my_seq4)
+my_seq4 = fib_seq(2000)
 
 # while len(fib_list) < stop:
 #     n = len(fib_list)
